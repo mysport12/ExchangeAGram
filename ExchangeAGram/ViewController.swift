c07ce4dd080524aadb9766d5715c3ca49201c8c3
Craig Martin <cmart1220@gmail.com>
2015-03-11 21:38:38 -0400

    Initial Commit

---
+++ ExchangeAGram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExchangeAGram
+//
+//  Created by Craig Martin on 3/11/15.
+//  Copyright (c) 2015 MadKitty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
